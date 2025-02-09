4ba85a5c8bdc54c36b6229df562082de9ec09eba
yogeshOzaDeanInfotech <o.yogesh@deaninfotech.com>
2025-02-09 15:44:20 +0530

    unmounting phase

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import React, { Component } from 'react'
 import Demo1 from './Demo1';
 export default class App extends Component {
-
+  
   constructor(){
-    
     super();
     this.state = {
-      count : 9,
+      isDelete : false
     }
   }
+
+
   render() {
-    
     return (
       <div>
-        <Demo1  count={this.state.count}/>
+        {this.state.isDelete ? null : <Demo1/>}
         <hr /> 
         <h2>App Component</h2>
-        <button onClick={()=> this.setState({count : this.state.count + 1})}>+</button>
+        <button onClick={()=> this.setState({isDelete : true})}>Remove</button>
       </div>
     )
   }
+
+
+
 }
--- src/Demo1.jsx
+++ src/Demo1.jsx
@@ -1,56 +1,14 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 export default class Demo1 extends Component {
 
-    constructor(props) {
-      console.log("constructor method is called....")
-        super(props);
-        this.state = {
-            count: 9,
-        };
-    }
-
-    static getDerivedStateFromProps(nextProps, prevState){
-      console.log("getDerivedStateFromProps method is called.....")
-      if(nextProps.count != prevState.count){
-        return { count : nextProps.count * nextProps.count }
-      }
-      return null;
-    }
-    
-    shouldComponentUpdate(nextProps, nextState){
-      console.log("shouldComponentUpdate method is called.....")
-      console.log(nextProps, nextState)
-      return true;
-    }
-
-    getSnapshotBeforeUpdate(prevProps, prevState) {
-      console.log("getSnapshotBeforeUpdate method is called .... ");
-      console.log(prevProps , prevState)
-      return prevProps; // Returns previous count
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-      if(snapshot.count == 12){
-        alert("you have won!!!!")
-      }
-      console.log("componentDidUpdate: Previous count was", snapshot);
-    }
-
-    render() {
-      console.log("render method is called.....")
-        return (
-            <div>
-                <h1>Demo Component</h1>
-                <h2>Count : {this.state.count}</h2>
-            </div>
-        );
-    }
-
-
-    componentDidMount(){
-      setTimeout(() => {
-        console.log("component is mounted")
-      },5000);
-    }
+  componentWillUnmount(){
+    console.log("componentWillUnmount method is callled....")
+    alert("are you sure ??")
+  }
+  render() {
+    return (
+      <div>this is folder</div>
+    )
+  }
 }
