71293aafe30d4d372ee2087b5b29a1b931102621
yogeshOzaDeanInfotech <o.yogesh@deaninfotech.com>
2025-02-10 22:16:52 +0530

    map and key in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import Child1 from "./Child1";
+
 
 const App = () =>{
+  const students = [
+    {id : 1 , name : "student1" , age : 13},
+    {id : 2 , name : "student2" , age : 14},
+    {id : 3 , name : "student3" , age : 15},
+    {id : 4 , name : "student4" , age : 11},
+    {id : 5 , name : "student5" , age : 9},
+  ]
+  
   return (
     <div>
-      <Child1>
-        <h1>Header</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi distinctio nam iure facere cumque dolorem obcaecati soluta ea, dolore, est aut blanditiis sed veniam quidem deleniti similique iusto, culpa numquam.</p>
-        <h2>Data</h2>
-      </Child1>
+        <h1>List Of students</h1>
+          {
+            students.map((s, index)=>(
+              <div key={index}>
+                <h2>{s.name}</h2>
+                <h4>{s.age}</h4>
+              </div>
+            ))
+          }
     </div>
   )
 }
 
 export default App;
+
+
+// student list in table format => 1 sr.no 2. name 3. age 4. address
